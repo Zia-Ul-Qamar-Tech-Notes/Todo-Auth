d7dfb873e25d28d2d5146738deb080f771d281de
Zia-Ul-Qamar-Tech-Notes <zia.softwareprogrammer@gmail.com>
2024-10-04 01:10:40 +0500

    database-path

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,10 @@ import { todoRouter } from "./routes/todo.js";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
-mongoose.connect("mongodb://localhost:27017/todoAuth");
+mongoose.connect(process.env.MONGODB_URI, {
+  dbName: "ToDo",
+  bufferCommands: false,
+});
 app.use(express.json());
 app.use(cors());
 app.use("/auth", UserRouter);
