0b773b741d11034bebd8e14eba4ffd2dfd0cccf6
Zia-Ul-Qamar-Tech-Notes <zia.softwareprogrammer@gmail.com>
2024-10-04 10:41:45 +0500

    env_configure

--- client/todo-Auth/src/pages/DisplayTodo.jsx
+++ client/todo-Auth/src/pages/DisplayTodo.jsx
@@ -4,6 +4,9 @@ import { useCookies } from "react-cookie";
 import { MdDeleteForever } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
+// Use the environment variable
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const DisplayTodo = () => {
   const [todos, settodos] = useState([]);
   const userId = window.localStorage.getItem("userId");
@@ -14,13 +17,13 @@ export const DisplayTodo = () => {
     const isCompleted = e.target.checked;
     settodos((prev) => {
       return prev.map((todo) =>
-        todo._id == id ? { ...todo, status: isCompleted } : todo
+        todo._id === id ? { ...todo, status: isCompleted } : todo
       );
     });
 
     try {
       const response = await axios.put(
-        `http://localhost:5000/todo/${id}`,
+        `${baseURL}/todo/${id}`, // Use the baseURL
         {
           status: isCompleted,
         },
@@ -37,10 +40,10 @@ export const DisplayTodo = () => {
   const deleteTodo = async (e, id) => {
     e.preventDefault();
     settodos((prev) => {
-      return prev.filter((todo) => todo._id != id);
+      return prev.filter((todo) => todo._id !== id);
     });
     try {
-      const response = await axios.delete(`http://localhost:5000/todo/${id}`, {
+      const response = await axios.delete(`${baseURL}/todo/${id}`, {
         headers: { authorize: cookies.access_token },
       });
       console.log(response);
@@ -50,14 +53,14 @@ export const DisplayTodo = () => {
   };
 
   const getTodo = async () => {
-    let userId = window.localStorage.getItem("userId");
     try {
-      const response = await axios.get(`http://localhost:5000/todo/${userId}`);
+      const response = await axios.get(`${baseURL}/todo/${userId}`);
       settodos(response.data);
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     if (!userId) {
       return navigate("/login");
@@ -69,52 +72,50 @@ export const DisplayTodo = () => {
   return (
     <>
       <div>
-        <h1 className=" text-center font-bold text-xl items-center">
+        <h1 className="text-center font-bold text-xl items-center">
           Display Todo
         </h1>
-        {todos.length != 0 ? (
+        {todos.length !== 0 ? (
           <table className="table-auto w-full rounded-md overflow-hidden">
             <thead className="bg-green-800 text-white">
               <tr>
-                <th className=" py-2">Tasks</th>
-                <th className=" py-2">Description</th>
-                <th className=" py-2">Status</th>
+                <th className="py-2">Tasks</th>
+                <th className="py-2">Description</th>
+                <th className="py-2">Status</th>
               </tr>
             </thead>
-            <tbody className=" bg-green-200">
-              {todos.map((todo) => {
-                return (
-                  <tr key={todo._id}>
-                    <td className=" p-3 border border-white text-center min-w-32">
-                      <div className="flex justify-center gap-2 items-center">
-                        <span>{todo.task}</span>
-                      </div>
-                    </td>
-                    <td className=" p-3 border border-white text-center min-w-32">
-                      <div className="flex justify-center gap-2 items-center">
-                        <span>{todo.description}</span>
-                      </div>
-                    </td>
-                    <td className=" p-3 border border-white text-center min-w-32">
-                      <div className="flex justify-center gap-2 items-center">
-                        <input
-                          type="checkbox"
-                          name="status"
-                          checked={todo.status}
-                          id="statusComplete"
-                          onChange={(e) => changeStatus(e, todo._id)}
-                        />
-                        {todo.status ? <p>Completed</p> : <p>In-Completed</p>}
-                        <MdDeleteForever
-                          title="Delete Todo"
-                          size={30}
-                          onClick={(e) => deleteTodo(e, todo._id)}
-                        />
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
+            <tbody className="bg-green-200">
+              {todos.map((todo) => (
+                <tr key={todo._id}>
+                  <td className="p-3 border border-white text-center min-w-32">
+                    <div className="flex justify-center gap-2 items-center">
+                      <span>{todo.task}</span>
+                    </div>
+                  </td>
+                  <td className="p-3 border border-white text-center min-w-32">
+                    <div className="flex justify-center gap-2 items-center">
+                      <span>{todo.description}</span>
+                    </div>
+                  </td>
+                  <td className="p-3 border border-white text-center min-w-32">
+                    <div className="flex justify-center gap-2 items-center">
+                      <input
+                        type="checkbox"
+                        name="status"
+                        checked={todo.status}
+                        id="statusComplete"
+                        onChange={(e) => changeStatus(e, todo._id)}
+                      />
+                      {todo.status ? <p>Completed</p> : <p>In-Completed</p>}
+                      <MdDeleteForever
+                        title="Delete Todo"
+                        size={30}
+                        onClick={(e) => deleteTodo(e, todo._id)}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         ) : (
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import mongoose, { connect } from "mongoose";
 import cors from "cors";
 import { UserRouter } from "./routes/user.js";
 import { todoRouter } from "./routes/todo.js";
@@ -8,10 +8,13 @@ import { todoRouter } from "./routes/todo.js";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGODB_URI, {
-  dbName: "ToDo",
-  bufferCommands: false,
-});
+const connectDB = async () =>
+  await mongoose.connect(process.env.MONGODB_URI, {
+    dbName: "ToDo",
+  });
+
+connectDB();
+
 app.use(express.json());
 app.use(cors());
 app.use("/auth", UserRouter);
